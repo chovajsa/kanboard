8c7f40d5a07cc261414c0681aa6b1e2741232a03
Miro Kanovsky <kanovskym@gmail.com>
2014-08-06 18:28:45 +0200

    archive

--- app/Templates/board_index.php
+++ app/Templates/board_index.php
@@ -3,6 +3,13 @@
     <?php if (empty($board)): ?>
         <p class="alert alert-error"><?= t('There is no column in your project!') ?></p>
     <?php else: ?>
+    	<?php 
+        foreach ($board as $v=>$n) {
+        	if (in_array($n['title'], array('Archive', 'Deleted'))) {
+        		unset($board[$v]);
+        	}
+        }
+        ?>
         <?= Helper\template('board_show', array('current_project_id' => $current_project_id, 'board' => $board, 'categories' => $categories)) ?>
     <?php endif ?>
 
