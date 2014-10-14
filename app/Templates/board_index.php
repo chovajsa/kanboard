c73f613e2f4372d055bd06e989896339646ab02a
Miro Kanovsky <kanovskym@gmail.com>
2014-10-14 16:43:22 +0200

    upravy

--- app/Templates/board_index.php
+++ app/Templates/board_index.php
@@ -4,6 +4,7 @@
         <p class="alert alert-error"><?= t('There is no column in your project!') ?></p>
     <?php else: ?>
     	<?php 
+        
         if (!isset($_GET['showHidden'])) foreach ($board as $v=>$n) {
         	if (in_array($n['title'], array('Archive', 'Deleted'))) {
         		unset($board[$v]);
